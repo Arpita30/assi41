dfe2d25d14716c67044478c81f8894c4a6d380e9
Arpita30 <arpita.salecha.jain@gmail.com>
2016-10-04 08:37:48 +0530

    Add files via upload

---
+++ Computer.java
@@ -0,0 +1,21 @@
+package Computer;
+
+public class Computer {
+	
+	String ramCapacity;
+	String diskCapacity;
+	
+	
+	protected String returnHardDiskCapacity(){
+		diskCapacity="512 GB";	
+		return diskCapacity;
+		
+	}
+	
+ 
+	/**
+	 * @param args
+	 */
+	
+
+}
---
+++ Laptop.java
@@ -0,0 +1,19 @@
+package Laptop;
+import Computer.Computer;
+public class Laptop extends Computer {
+
+	
+	
+    
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Laptop l= new Laptop();
+		
+		System.out.println("hard disk capacity of laptop is:-> "+l.returnHardDiskCapacity());
+	}
+
+}
